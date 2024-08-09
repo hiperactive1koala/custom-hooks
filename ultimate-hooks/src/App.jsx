d29196c3da620bfdbf5101c43bbb2164c66fad93
Ismayil Shirinov <sunqurlar@gmail.com>
2024-08-09 22:23:54 +0300

    End of 7B

--- ultimate-hooks/src/App.jsx
+++ ultimate-hooks/src/App.jsx
@@ -1,37 +1,6 @@
-import { useState, useEffect } from 'react'
-import axios from 'axios'
 
-const useField = (type) => {
-  const [value, setValue] = useState('')
-
-  const onChange = (event) => {
-    setValue(event.target.value)
-  }
-
-  return {
-    type,
-    value,
-    onChange
-  }
-}
-
-const useResource = (baseUrl) => {
-  const [resources, setResources] = useState([])
-
-  // ...
-
-  const create = (resource) => {
-    // ...
-  }
-
-  const service = {
-    create
-  }
-
-  return [
-    resources, service
-  ]
-}
+import { useEffect } from 'react'
+import { useField, useResource } from './hooks'
 
 const App = () => {
   const content = useField('text')
@@ -41,9 +10,15 @@ const App = () => {
   const [notes, noteService] = useResource('http://localhost:3005/notes')
   const [persons, personService] = useResource('http://localhost:3005/persons')
 
+  useEffect(() => {
+    noteService.getAll()
+    personService.getAll()
+  }, [])
+  
+
   const handleNoteSubmit = (event) => {
     event.preventDefault()
-    noteService.create({ content: content.value })
+    noteService.create({ content: content.value })    
   }
  
   const handlePersonSubmit = (event) => {
@@ -55,15 +30,15 @@ const App = () => {
     <div>
       <h2>notes</h2>
       <form onSubmit={handleNoteSubmit}>
-        <input {...content} />
+        <input name='note' {...content} />
         <button>create</button>
       </form>
       {notes.map(n => <p key={n.id}>{n.content}</p>)}
 
       <h2>persons</h2>
       <form onSubmit={handlePersonSubmit}>
-        name <input {...name} /> <br/>
-        number <input {...number} />
+        name <input name='name' {...name} /> <br/>
+        number <input name='number' {...number} />
         <button>create</button>
       </form>
       {persons.map(n => <p key={n.id}>{n.name} {n.number}</p>)}
